24b2920e6ba5c3acd7f12352cab4eeebef6728d1
Catalin <IancuCatalinCristian94@gmail.com>
2022-10-27 19:40:06 +0300

    Adding OperationTypes Model

---
+++ ExchangeApi/Models/OperationTypes.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExchangeApi.Models
+{
+    public class OperationTypes
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
