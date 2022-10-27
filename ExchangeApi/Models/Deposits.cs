9af7bd3b23b0e610039275f624d75a89de4b3f5a
Catalin <IancuCatalinCristian94@gmail.com>
2022-10-27 19:51:20 +0300

    Adding Deposits Model

---
+++ ExchangeApi/Models/Deposits.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExchangeApi.Models
+{
+    public class Deposits
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public decimal? Amount { get; set; }
+        public string? FromAdress { get; set; }
+    }
+}
