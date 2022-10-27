7ea6ebb22286a414b20782b3c1041387215364e6
Catalin <IancuCatalinCristian94@gmail.com>
2022-10-27 20:17:32 +0300

    Adding TraderOrderTypes Model

---
+++ ExchangeApi/Models/TradeOrderTypes.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExchangeApi.Models
+{
+    public class TradeOrderTypes
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
