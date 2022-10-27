9019e4620d1df7cc6c6ad7bcffe064dca0339259
Catalin <IancuCatalinCristian94@gmail.com>
2022-10-27 19:47:38 +0300

    Adding Operations Model

---
+++ ExchangeApi/Models/Operations.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExchangeApi.Models
+{
+    public class Operations
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public decimal? Amount { get; set; }
+    }
+}
