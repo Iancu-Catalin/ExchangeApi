8b2aa0d251aa24844d5cd0bad0dc17d3159129f9
Catalin <IancuCatalinCristian94@gmail.com>
2022-10-27 20:07:32 +0300

    Adding Withdrawals Model

---
+++ ExchangeApi/Models/Withdrawals.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExchangeApi.Models
+{
+    public class Withdrawals
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public decimal? Amount { get; set; }
+        public bool? WasApprovedByUser2fA { get; set; }
+        public string? ToAdress { get; set; }
+    }
+}
