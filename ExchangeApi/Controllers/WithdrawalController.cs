1ffd3447701f96be2f418f59af45936f4e56ab54
Catalin <IancuCatalinCristian94@gmail.com>
2022-10-28 16:34:41 +0300

    Adding Withdrawal Controller

---
+++ ExchangeApi/Controllers/WithdrawalController.cs
@@ -0,0 +1,32 @@
+﻿using ExchangeApi.Services;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WithdrawalController : ControllerBase
+    {
+        private readonly IExchangeService _exchangeService;
+
+        public WithdrawalController(IExchangeService exchangeService)
+        {
+            _exchangeService = exchangeService;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetWithdrawals()
+        {
+            var withdrawals = await _exchangeService.GetWithdrawalsAsync();
+
+            if (withdrawals == null)
+            {
+                return StatusCode(StatusCodes.Status204NoContent, "There are no Withdrawals.");
+            }
+
+            return StatusCode(StatusCodes.Status200OK, withdrawals);
+        }
+    }
+}
