d7e181e30004545929562b74cc232116f5288acf
Catalin <IancuCatalinCristian94@gmail.com>
2022-10-28 16:26:23 +0300

    Adding Trade Order Type Controller

---
+++ ExchangeApi/Controllers/TradeOrderTypeController.cs
@@ -0,0 +1,31 @@
+﻿using ExchangeApi.Services;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TradeOrderTypeController : ControllerBase
+    {
+        private readonly IExchangeService _exchangeService;
+
+        TradeOrderTypeController(IExchangeService exchangeService)
+        {
+            _exchangeService = exchangeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTradeOrderTypes()
+        {
+            var tradeOrderTypes = await _exchangeService.GetTradeOrderTypesAsync();
+
+            if (tradeOrderTypes == null)
+            {
+                return StatusCode(StatusCodes.Status204NoContent, "There are no Trade Order Types.");
+            }
+
+            return StatusCode(StatusCodes.Status200OK, tradeOrderTypes);
+        }
+    }
+}
