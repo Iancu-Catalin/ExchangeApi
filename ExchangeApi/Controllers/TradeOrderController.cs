6f5f90fab13ec1c60eab62ac8934402d63d485d5
Catalin <IancuCatalinCristian94@gmail.com>
2022-10-28 16:14:51 +0300

    Adding Trade Order Controller

---
+++ ExchangeApi/Controllers/TradeOrderController.cs
@@ -0,0 +1,31 @@
+﻿using ExchangeApi.Services;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TradeOrderController : ControllerBase
+    {
+        private readonly IExchangeService _exchangeService;
+
+        public TradeOrderController(IExchangeService exchangeService)
+        {
+            _exchangeService = exchangeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTradeOrders()
+        {
+            var tradeOrders = await _exchangeService.GetTradeOrdersAsync();
+
+            if (tradeOrders == null)
+            {
+                return StatusCode(StatusCodes.Status204NoContent, "There are no Trade Orders");
+            }
+
+            return StatusCode(StatusCodes.Status200OK, tradeOrders);
+        }
+    }
+}
