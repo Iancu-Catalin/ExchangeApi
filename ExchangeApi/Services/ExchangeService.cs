469d23dc2a6bfeec972029ef6e827b07fa254c70
Catalin <IancuCatalinCristian94@gmail.com>
2022-10-28 14:57:59 +0300

    Adding Exchange Service & IEchangeService changes

---
+++ ExchangeApi/Services/ExchangeService.cs
@@ -0,0 +1,89 @@
+﻿using ExchangeApi.Data;
+using ExchangeApi.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace ExchangeApi.Services
+{
+    public class ExchangeService : IExchangeService
+    {
+        private readonly AppDbContext _db;
+
+        public ExchangeService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<List<Deposit>> GetDepositsAsync()
+        {
+            try
+            {
+                return await _db.Deposits.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task<List<Operation>> GetOperationsAsync()
+        {
+            try
+            {
+                return await _db.Operations.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task<List<OperationType>> GetOperationTypesAsync()
+        {
+            try
+            {
+                return await _db.OperationTypes.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task<List<TradeOrder>> GetTradeOrdersAsync()
+        {
+            try
+            {
+                return await _db.TradeOrders.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task<List<TradeOrderType>> GetTradeOrderTypesAsync()
+        {
+            try
+            {
+                return await _db.TradeOrderTypes.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task<List<Withdrawal>> GetWithdrawalsAsync()
+        {
+            try
+            {
+                return await _db.Withdrawals.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
--- ExchangeApi/Services/IExchangeService.cs
+++ ExchangeApi/Services/IExchangeService.cs
@@ -4,12 +4,11 @@ namespace ExchangeApi.Services
 {
     public interface IExchangeService
     {
-        Task<List<OperationType>> GetOperationTypes(); // Will get all the operation types
-        Task<List<Operation>> GetOperations(); // Will get all the operations
-        Task<List<Withdrawal>> GetWithdrawals(); // Will get all the withdrawals
-        Task<List<Deposit>> GetDeposits(); // Will get all the deposits
-        Task<List<TradeOrder>> GetTradeOrders(); // Will get all the Trade Orders
-        Task<List<TradeOrderType>> GetTradeOrderTypes(); // Will get all the Trade Order Types
-
+        Task<List<OperationType>> GetOperationTypesAsync(); // Will get all the operation types
+        Task<List<Operation>> GetOperationsAsync(); // Will get all the operations
+        Task<List<Withdrawal>> GetWithdrawalsAsync(); // Will get all the withdrawals
+        Task<List<Deposit>> GetDepositsAsync(); // Will get all the deposits
+        Task<List<TradeOrder>> GetTradeOrdersAsync(); // Will get all the Trade Orders
+        Task<List<TradeOrderType>> GetTradeOrderTypesAsync(); // Will get all the Trade Order Types
     }
 }
