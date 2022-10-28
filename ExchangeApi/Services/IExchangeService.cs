6165ec87312490e79ec1d3b1166be6363763fc4a
Catalin <IancuCatalinCristian94@gmail.com>
2022-10-28 13:58:34 +0300

    Adding IExchangeService repository

---
+++ ExchangeApi/Services/IExchangeService.cs
@@ -0,0 +1,15 @@
+﻿using ExchangeApi.Models;
+
+namespace ExchangeApi.Services
+{
+    public interface IExchangeService
+    {
+        Task<List<OperationType>> GetOperationTypes(); // Will get all the operation types
+        Task<List<Operation>> GetOperations(); // Will get all the operations
+        Task<List<Withdrawal>> GetWithdrawals(); // Will get all the withdrawals
+        Task<List<Deposit>> GetDeposits(); // Will get all the deposits
+        Task<List<TradeOrder>> GetTradeOrders(); // Will get all the Trade Orders
+        Task<List<TradeOrderType>> GetTradeOrderTypes(); // Will get all the Trade Order Types
+
+    }
+}
