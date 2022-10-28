f1f438da65eb56383a6eea0cc7d12274e0f31318
Catalin <IancuCatalinCristian94@gmail.com>
2022-10-28 15:23:01 +0300

    Adding Deposit Controller

---
+++ ExchangeApi/Controllers/DepositController.cs
@@ -0,0 +1,31 @@
+﻿using ExchangeApi.Services;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepositController : ControllerBase
+    {
+        private readonly IExchangeService _exchangeService;
+
+        public DepositController(IExchangeService exchangeService)
+        {
+            _exchangeService = exchangeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDeposits()
+        {
+            var deposits = await _exchangeService.GetDepositsAsync();
+
+            if (deposits == null)
+            {
+                return StatusCode(StatusCodes.Status204NoContent, "There are no deposits.");
+            }
+
+            return StatusCode(StatusCodes.Status200OK, deposits);
+        }
+    }
+}
